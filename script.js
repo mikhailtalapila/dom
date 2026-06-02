4fd71e382269ea7a93005c451f0fee835c0e73e0
Michael Talapila <11203122+mikhailtalapila@users.noreply.github.com>
2026-06-02 18:26:00 -0400

    Prevent default - stop propagation

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-const container = document.querySelector("#container");
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the text-content";
-container.appendChild(content);
-const p = document.createElement("p");
-p.style.color = "red";
-p.textContent = "Hey I'm red";
-container.appendChild(p);
-const h3 = document.createElement("h3");
-h3.style.color = "blue";
-h3.textContent = "I'm a blue h3!";
-container.appendChild(h3);
-const div = document.createElement("div");
-div.style.border = "black solid 2px";
-div.style["background-color"] = "pink";
-const h1 = document.createElement("h1");
-h1.textContent = "I'm in a div";
-div.appendChild(h1);
-const p2 = document.createElement("p");
-p2.textContent = "ME TOO!";
-div.appendChild(p2);
-container.appendChild(div);
+const link = document.querySelector("a");
+link.addEventListener("click", function (e) {
+  console.log("you clicked the link");
+  e.stopPropagation();
+  e.preventDefault();
+});
+const btn = document.querySelector("#one");
+btn.addEventListener("click", function (e) {
+  console.log("you clicked the button");
+  e.stopPropagation();
+});
+
+document.body.addEventListener("click", function (event) {
+  console.log("you clicked the body");
+});
