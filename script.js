7091dd6e0a8e033bad71f0c5da9dc62d49dd941e
Michael Talapila <11203122+mikhailtalapila@users.noreply.github.com>
2026-06-03 18:27:33 -0400

    Add another clickable event

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const clickEvent = new Event("click");
-const btn = document.getElementById("one");
+let clickEvent = new MouseEvent("click", {
+  bubbles: true,
+  cancelable: true,
+  clientX: 150,
+  clientY: 150,
+});
+
+const btn = document.querySelector("#one");
 btn.addEventListener("click", function (event) {
-  console.log(event);
+  console.log(`Event has occurred at X:${event.clientX}, Y:${event.clientY}`);
   console.log("clicked");
 });
 
