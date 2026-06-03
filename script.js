4617178f36f11e8c4589b56ee810b84c053ce0da
Michael Talapila <11203122+mikhailtalapila@users.noreply.github.com>
2026-06-03 19:00:57 -0400

    Add custom events

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-let clickEvent = new MouseEvent("click", {
-  bubbles: true,
-  cancelable: true,
-  clientX: 150,
-  clientY: 150,
+function highlight(elem) {
+  elem.style.backgroundColor = "yellow";
+  const customEvent = new CustomEvent("mark", {
+    detail: {
+      someRandomCustomDetail: "yellow background",
+    },
+  });
+  elem.dispatchEvent(customEvent);
+}
+
+const div = document.querySelector(".note");
+function addBorder(elem) {
+  elem.style.border = "solid black 3px";
+}
+div.addEventListener("mark", function (event) {
+  addBorder(this);
+  console.log(event.detail);
 });
 
-const btn = document.querySelector("#one");
-btn.addEventListener("click", function (event) {
-  console.log(`Event has occurred at X:${event.clientX}, Y:${event.clientY}`);
-  console.log("clicked");
-});
-
-btn.dispatchEvent(clickEvent);
+highlight(div);
