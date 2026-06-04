9ff58d50c7cb3ce8c79de514ca28377d93c373fd
Michael Talapila <11203122+mikhailtalapila@users.noreply.github.com>
2026-06-03 21:33:00 -0400

    Map function

--- script.js
+++ script.js
@@ -1,20 +1,89 @@
-function highlight(elem) {
-  elem.style.backgroundColor = "yellow";
-  const customEvent = new CustomEvent("mark", {
-    detail: {
-      someRandomCustomDetail: "yellow background",
-    },
-  });
-  elem.dispatchEvent(customEvent);
+function funkyFunction(music, isWhiteBoy) {
+  if (isWhiteBoy) {
+    console.log(`Play: ` + music);
+  }
 }
 
-const div = document.querySelector(".note");
-function addBorder(elem) {
-  elem.style.border = "solid black 3px";
-}
-div.addEventListener("mark", function (event) {
-  addBorder(this);
-  console.log(event.detail);
+funkyFunction("that funky music", true);
+
+const secondFunc = function (music, isWhiteBoy) {
+  if (isWhiteBoy) {
+    console.log(`Play: ` + music);
+  }
+};
+
+const thirdFunc = (music, isWhiteBoy) => {
+  if (isWhiteBoy) {
+    console.log(`play: ` + music);
+  }
+};
+
+const playThe = (funky) => {
+  return funky + "music";
+};
+
+const playThe2 = (funky) => funky + "music";
+
+const playThe3 = (funky) => {
+  return funky + "music";
+};
+
+const notes = ["do", "re", "mi"];
+notes.forEach((note) => console.log(note));
+
+notes.forEach((note) => {
+  console.log(note);
 });
 
-highlight(div);
+notes.forEach(function (note) {
+  console.log(note);
+});
+
+notes.forEach(console.log);
+
+function myForeach(items, callback) {
+  for (let item of items) {
+    callback(item);
+  }
+}
+
+const myArr = [1, 2, 3, 4];
+myForeach(myArr, (item) => {
+  console.log(item);
+});
+
+myArr.forEach((item) => {
+  console.log(item);
+});
+
+function myForeach2(items, callback) {
+  for (let item of items) {
+    callback(item);
+  }
+}
+
+function myForeach(items, callback) {
+  for (let i = 0; i < items.length; i++) {
+    callback(items[i]);
+  }
+}
+
+function printPlusTwo(item) {
+  console.log(item + 2);
+}
+
+const myArr2 = [12, 3, 4, 5, 6];
+myForeach(myArr2, printPlusTwo);
+
+function myMap(array, callback) {
+  const myNewArray = [];
+  for (let item of array) {
+    let result = callback(item);
+    myNewArray.push(result);
+  }
+  return myNewArray;
+}
+
+const myArr3 = [1, 2, 3, 4];
+const myArr4 = myMap(myArr3, (item) => item * 5);
+console.log(myArr4);
